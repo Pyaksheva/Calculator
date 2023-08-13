52de0ee1ef8b1d412f0952fdd8af2239a47088a3
elena <ele-emelina@yandex.ru>
2023-08-13 17:37:42 +0300

    Initial Commit

---
+++ src/main/java/ru/piaksheva/calcapp/service/ResultWriterService.java
@@ -0,0 +1,7 @@
+package ru.piaksheva.calcapp.service;
+
+public class ResultWriterService {
+    public static void printResult(Integer a, Integer b, Integer c, String nameOperation) {
+            System.out.println(nameOperation + " двух чисел " + a + " и " + b + " = " + c);
+    }
+}
---
+++ src/main/java/ru/piaksheva/calcapp/util/Calculator.java
@@ -0,0 +1,14 @@
+package ru.piaksheva.calcapp.util;
+
+public class Calculator {
+
+    public static Integer summarize(Integer a, Integer b){
+        return a+b;
+    }
+    public static Integer subtract(Integer a, Integer b){
+        return a-b;
+    }
+    public static Integer multiply(Integer a, Integer b){
+        return a*b;
+    }
+}
